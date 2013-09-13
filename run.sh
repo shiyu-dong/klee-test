4af4743aeeebfbb2e926ba7bf778a17acb133cf5
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-09-13 17:48:47 +0000

    add more information

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 if [ "$1" == "--clean" ]
 then
   ./compare-optimization.sh --clean
-  rm *.txt
+  rm result*.txt output.txt
 else
   ./compare-optimization.sh $1 > output.txt
   ./get-result.py > result-$1.txt
