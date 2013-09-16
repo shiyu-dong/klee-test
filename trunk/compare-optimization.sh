eeca428b34b70d902929e716e4342c3bbd1f2370
ozzyo86@gmail.com <ozzyo86@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-09-16 22:05:46 +0000

    changed optimization example

--- trunk/compare-optimization.sh
+++ trunk/compare-optimization.sh
@@ -5,14 +5,16 @@ then
   echo "=============================================="
   echo "No Optimization "
   echo "=============================================="
-  klee --max-time=10 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false ../$1.bc -r --sym-files 20 1
+
+  klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false  ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1 
   klee-stats --print-all ../klee-last
 
   # with optimization but no flag
   echo "=============================================="
   echo "Empty Optimization, no flag"
   echo "=============================================="
-  klee --max-time=10 -libc=uclibc --posix-runtime ---use-cache=false --use-cex-cache=false -optimize ../$1.bc --sym-files 20 1
+  klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false  --optimize ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
+
   klee-stats --print-all ../klee-last
 
   #compare different optimizations for one program
@@ -52,7 +54,8 @@ then
     echo "=============================================="
     echo "Optimization with flag: "${OPT}
     echo "=============================================="
-    klee --max-time=10 -libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates,${OPT} ../$1.bc --sym-files 20 1
+  klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates,${OPT} ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1 
+
     klee-stats --print-all ../klee-last
   done
 fi
