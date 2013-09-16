13e8cd34b3d722af9e2b8df4ba674dd767c8c4bb
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-09-16 23:15:07 +0000

    change compare-optimization.sh to support 2+2

--- trunk/compare-optimization.sh
+++ trunk/compare-optimization.sh
@@ -6,18 +6,24 @@ then
   echo "No Optimization "
   echo "=============================================="
 
-  klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false  ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1 
-  klee-stats --print-all ../klee-last
+  klee.cde --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false  ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
+  klee-stats.cde --print-all ../klee-last
 
   # with optimization but no flag
   echo "=============================================="
   echo "Empty Optimization, no flag"
   echo "=============================================="
-  klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false  --optimize ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
+  klee.cde --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false  --optimize ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
+  klee-stats.cde --print-all ../klee-last
 
-  klee-stats --print-all ../klee-last
+  # with two memory optimization only
+  echo "=============================================="
+  echo "Two Memory Optimization only"
+  echo "=============================================="
+  klee.cde --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
+  klee-stats.cde --print-all ../klee-last
 
-  #compare different optimizations for one program
+  # with 2+1
   for OPT in \
     AggressiveDCE \
     ArgumentPromotion \
@@ -52,10 +58,87 @@ then
     TailCallElimination
   do
     echo "=============================================="
-    echo "Optimization with flag: "${OPT}
+    echo "Optimization with 2 + flag: "${OPT}
     echo "=============================================="
-  klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates,${OPT} ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1 
+    klee.cde --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates,${OPT} ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
+    klee-stats.cde --print-all ../klee-last
+  done
 
-    klee-stats --print-all ../klee-last
+  # with 2+2
+  for OPT1 in \
+    AggressiveDCE \
+    ArgumentPromotion \
+    CFGSimplification \
+    CondPropagation \
+    ConstantMerge \
+    DeadArgElimination \
+    DeadStoreElimination \
+    DeadTypeElimination \
+    FunctionAttrs \
+    FunctionInlining \
+    GlobalDCE \
+    GlobalOptimizer \
+    GVN \
+    IndVarSimplify \
+    InstructionCombining \
+    IPConstantPropagation \
+    JumpThreading \
+    LICM \
+    LoopDeletion \
+    LoopIndexSplit \
+    LoopRotate \
+    LoopUnroll \
+    LoopUnswitch \
+    MemCpyOpt \
+    PruneEH \
+    RaiseAllocation \
+    Reassociate \
+    SCCP \
+    SimplifyLibCalls \
+    StripDeadPrototypes \
+    TailCallElimination
+  do
+    for OPT2 in \
+      AggressiveDCE \
+      ArgumentPromotion \
+      CFGSimplification \
+      CondPropagation \
+      ConstantMerge \
+      DeadArgElimination \
+      DeadStoreElimination \
+      DeadTypeElimination \
+      FunctionAttrs \
+      FunctionInlining \
+      GlobalDCE \
+      GlobalOptimizer \
+      GVN \
+      IndVarSimplify \
+      InstructionCombining \
+      IPConstantPropagation \
+      JumpThreading \
+      LICM \
+      LoopDeletion \
+      LoopIndexSplit \
+      LoopRotate \
+      LoopUnroll \
+      LoopUnswitch \
+      MemCpyOpt \
+      PruneEH \
+      RaiseAllocation \
+      Reassociate \
+      SCCP \
+      SimplifyLibCalls \
+      StripDeadPrototypes \
+      TailCallElimination
+    do
+      if [ ${OPT1} != ${OPT2} ]
+      then
+        echo "=============================================="
+        echo "Optimization with 2 + flag: "${OPT1} ","${OPT2}
+        echo "=============================================="
+        klee.cde --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates,${OPT1},${OPT2} ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
+        klee-stats.cde --print-all ../klee-last
+      fi
+    done
   done
 fi
