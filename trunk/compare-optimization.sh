351756f5962854d3e4306bee15757e32c156e8f9
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-09-17 01:08:04 +0000

    further fix for compare-optimization.sh

--- trunk/compare-optimization.sh
+++ trunk/compare-optimization.sh
@@ -3,7 +3,7 @@ if [ "$1" != "--clean" ]
 then
   # without optimization
   echo "=============================================="
-  echo "No Optimization "
+  echo "no optimization "
   echo "=============================================="
 
   klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false  ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
@@ -11,14 +11,14 @@ then
 
   # with optimization but no flag
   echo "=============================================="
-  echo "Empty Optimization, no flag"
+  echo "only --optimize, no --opt-flag"
   echo "=============================================="
   klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false  --optimize ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
   klee-stats --print-all ../klee-last
 
   # with two memory optimization only
   echo "=============================================="
-  echo "Two Memory Optimization only"
+  echo "with two memory optimization only"
   echo "=============================================="
   klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
   klee-stats --print-all ../klee-last
@@ -58,7 +58,7 @@ then
     TailCallElimination
   do
     echo "=============================================="
-    echo "Optimization with 2 + flag: "${OPT}
+    echo "with two memory optimizations and flag "${OPT}
     echo "=============================================="
     klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates,${OPT} ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
     klee-stats --print-all ../klee-last
@@ -134,7 +134,7 @@ then
       if [ ${OPT1} != ${OPT2} ]
       then
         echo "=============================================="
-        echo "Optimization with 2 + flag: "${OPT1} ","${OPT2}
+        echo "with two optimizations and flags "${OPT1}","${OPT2}
         echo "=============================================="
         klee --max-time 120 --libc=uclibc --posix-runtime --use-cache=false --use-cex-cache=false --optimize --opt-flag=PromoteMemoryToRegister,ScalarReplAggregates,${OPT1},${OPT2} ../$1.bc --sym-args 1 10 10 --sym-files 2 2000 --max-fail 1
         klee-stats --print-all ../klee-last
