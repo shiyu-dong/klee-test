3a479fd6b1c6712bbdaf6c2870eee1fc23e835fd
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-09-18 17:59:49 +0000

    working script to test tr and echo

--- get-result.py
+++ get-result.py
@@ -3,19 +3,20 @@ import re
 import sys
 
 # read on target log
-fo = open("output.txt","r")
+fo = open(sys.argv[1],"r")
 
 while True:
   # extract message from the on target log to log
   line = fo.readline()
   if len(line) == 0:
     break;
+
   elif line.find("==========") != -1:
+    sys.stdout.write(line)
     while True:
-      sys.stdout.write(line)
       line = fo.readline()
-      if line.find("=========") == -1:
-        sys.stdout.write(line)
+      sys.stdout.write(line)
+      if line.find("=========") != -1:
         break
 
   elif line.find("KLEE: done") != -1:
@@ -23,12 +24,15 @@ while True:
 
   elif line.find("----------") != -1:
     sys.stdout.write(line)
-    line = fo.readline()
-    sys.stdout.write(line)
-    line = fo.readline()
-    sys.stdout.write(line)
-    line = fo.readline()
-    sys.stdout.write(line)
-    line = fo.readline()
-    sys.stdout.write(line)
+    row = 0
+    while True:
+      line = fo.readline()
+      if line.find("---------") != -1 and row >= 2:
+        sys.stdout.write(line)
+        sys.stdout.write("\n")
+        break
+      else:
+        sys.stdout.write(line)
+        row += 1
+
 fo.close()
