64c84630d12a42e8f5e65f5472ecb3057af1a6c5
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-09-30 17:40:32 +0000

    add new supported program

--- trunk/analysis.sh
+++ trunk/analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROGRAMS="base64 basename date df touch"
+PROGRAMS="base64 basename cat chcon date df touch"
 for program in $PROGRAMS
 do
   ./get-result.py test_result_saved/result_$program.txt
--- trunk/check.sh
+++ trunk/check.sh
@@ -1,12 +1,13 @@
-#PROGRAMS="base64 basename cat chcon chgrp chmod chown chroot cksum comm cp csplit cut \
+#PROGRAMS="base64 basename cat chcon chmod chown chroot cksum comm cp csplit cut \
 #  date dd df dircolors dirname du echo env expand expr factor false fmt fold \
 #  head hostid hostname id ginstall join kill link ln logname ls md5sum mkdir \
 #  mkfifo mknod mktemp mv nice nl nohup od paste pathchk pinky pr printenv printf \
 #  ptx pwd readlink rm rmdir runcon seq setuidgid shred shuf sleep sort split \
-#  stat stty sum sync tac tail tee touch tr tsort tty uname unexpand uniq unlink \
+#  stat stty sum sync tac tail tee tr tsort tty uname unexpand uniq unlink \
 #  uptime users wc whoami who yes"
 #PROGRAMS="basename date df su touch"
-PROGRAMS="base64 cat chcon chgrp chmod chown chroot cksum comm cp csplit cut \
+# Not working: chgrp, touch
+PROGRAMS=" chmod chown chroot cksum comm cp csplit cut \
   dd dircolors dirname du echo env expand expr factor false fmt fold \
   head hostid hostname id ginstall join kill link ln logname ls md5sum mkdir \
   mkfifo mknod mktemp mv nice nl nohup od paste pathchk pinky pr printenv printf \
