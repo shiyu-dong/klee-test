06479dc8ce7ee2e07daa916797665bb5cfacdfcb
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-10-10 20:54:11 +0000

    commit test results also

--- trunk/analysis.sh
+++ trunk/analysis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PROGRAMS="seq echo fold chgrp chown readlink uname"
+#PROGRAMS="seq echo fold chgrp chown readlink uname"
+PROGRAMS="base64 cat chcon comm date df dircolors dirname factor mknod tr pr cut chgrp chown echo fold readlink seq uname"
 for program in $PROGRAMS
 do
   ./get-result.py test_result_new/result_$program.txt
