ea154eeb3d5513cf89249623a7fb57115a0ca920
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-10-10 21:52:44 +0000

    adding new test fix result

--- recheck.sh
+++ recheck.sh
@@ -1,5 +1,6 @@
 if [ ! -d "test_result" ]; then
   mkdir test_result
 fi
-./replay.sh tr DeadArgElimination &> test_result/result_tr_DeadArgElimination.txt
-./replay.sh echo RaiseAllocation &> test_result/result_echo_RaiseAllocation.txt
+./replay.sh tr SimplifyLibCalls &> test_result/result_tr_SimplifyLibCalls.txt
+./replay.sh pr GlobalOptimizer &> test_result/result_pr_GlobalOptimizer.txt
+./replay.sh pr InstructionCombining &> test_result/result_pr_InstructionCombining.txt
