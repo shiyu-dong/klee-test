ea8e54ac32352b6554732131381645377b254b85
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-10-15 05:29:45 +0000

    add test result fix

--- recheck.sh
+++ recheck.sh
@@ -1,6 +1,20 @@
+FLAG2="\
+aiseAllocation,\
+ScalarReplAggregates,\
+MemCpyOpt,\
+InstructionCombining,\
+DeadStoreElimination,\
+DeadTypeElimination,\
+FunctionAttrs,\
+GlobalDCE,\
+IPConstantPropagation,\
+LoopDeletion,\
+LoopRotate,\
+PruneEH,\
+Reassociate,\
+StripDeadPrototypes"
+
 if [ ! -d "test_result" ]; then
   mkdir test_result
 fi
-./replay.sh tr SimplifyLibCalls &> test_result/result_tr_SimplifyLibCalls.txt
-./replay.sh pr GlobalOptimizer &> test_result/result_pr_GlobalOptimizer.txt
-./replay.sh pr InstructionCombining &> test_result/result_pr_InstructionCombining.txt
+./replay.sh chgrp ${FLAG2} &> test_result/result_chgrp.2.txt
