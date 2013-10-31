2a12839d1a032005afdbae9663312348f9555fe1
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-10-31 18:31:52 +0000

    update scripts

---
+++ compare-all.sh
@@ -0,0 +1,122 @@
+FLAG1="\
+RaiseAllocation,\
+ScalarReplAggregates,\
+MemCpyOpt,\
+InstructionCombining,\
+CondPropagation,\
+DeadArgElimination,\
+DeadStoreElimination,\
+DeadTypeElimination,\
+FunctionAttrs,\
+GlobalDCE,\
+GVN,\
+IPConstantPropagation,\
+LICM,\
+LoopDeletion,\
+LoopIndexSplit,\
+LoopRotate,\
+LoopUnswitch,\
+PruneEH,\
+Reassociate,\
+SimplifyLibCalls,\
+SCCP,\
+StripDeadPrototypes,\
+CFGSimplification"
+
+FLAG2="\
+aiseAllocation,\
+ScalarReplAggregates,\
+MemCpyOpt,\
+InstructionCombining,\
+DeadStoreElimination,\
+DeadTypeElimination,\
+FunctionAttrs,\
+GlobalDCE,\
+IPConstantPropagation,\
+LoopDeletion,\
+LoopRotate,\
+PruneEH,\
+Reassociate,\
+StripDeadPrototypes"
+
+rm -rf ../klee-out-* ../klee-last
+if [ "$1" != "--clean" ]
+then
+  # without optimization
+  echo "=============================================="
+  echo "no optimization "
+  echo "=============================================="
+  ./run-a-test.sh $1
+  klee-stats --print-all ../klee-last
+
+  cd ../../../obj-gcov/src/
+  rm -rf *.gcda
+  klee-replay $1 ../../obj-llvm/src/klee-last/*.ktest
+  gcov -b -c $1
+  cd ../../obj-llvm/src/klee-test
+
+  # with original klee optimization
+  echo "=============================================="
+  echo "original optimization "
+  echo "=============================================="
+  ./run-a-test.sh $1 --optimize
+  klee-stats --print-all ../klee-last
+
+  cd ../../../obj-gcov/src/
+  rm -rf *.gcda
+  klee-replay $1 ../../obj-llvm/src/klee-last/*.ktest
+  gcov -b -c $1
+  cd ../../obj-llvm/src/klee-test
+
+  # with one opt-flag
+  for OPT in \
+    ${FLAG1} \
+    ${FLAG2}
+    # AggressiveDCE \
+    # ArgumentPromotion \
+    # CFGSimplification \
+    # CondPropagation \
+    # ConstantMerge \
+    # DeadArgElimination \
+    # DeadStoreElimination \
+    # DeadTypeElimination \
+    # FunctionAttrs \
+    # FunctionInlining \
+    # GlobalDCE \
+    # GlobalOptimizer \
+    # GVN \
+    # IndVarSimplify \
+    # InstructionCombining \
+    # IPConstantPropagation \
+    # JumpThreading \
+    # LICM \
+    # LoopDeletion \
+    # LoopIndexSplit \
+    # LoopRotate \
+    # LoopUnroll \
+    # LoopUnswitch \
+    # MemCpyOpt \
+    # PromoteMemoryToRegister \
+    # PruneEH \
+    # RaiseAllocation \
+    # Reassociate \
+    # ScalarReplAggregates \
+    # SCCP \
+    # SimplifyLibCalls \
+    # StripDeadPrototypes \
+    # TailCallElimination
+  do
+    echo "=============================================="
+    echo "with optimization flag "${OPT}
+    echo "=============================================="
+    ./run-a-test.sh $1 --optimize --opt-flag=${OPT}
+    klee-stats --print-all ../klee-last
+
+    cd ../../../obj-gcov/src/
+    rm -rf *.gcda
+    klee-replay $1 ../../obj-llvm/src/klee-last/*.ktest
+    gcov -b -c $1
+    cd ../../obj-llvm/src/klee-test
+
+  done
+fi
