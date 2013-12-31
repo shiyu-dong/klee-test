39423ec28f31ac8296f98823f17e9b162d632252
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2013-12-31 09:51:35 +0000

    scripts for more time and less flag

--- trunk/analysis.sh
+++ trunk/analysis.sh
@@ -2,7 +2,12 @@
 #PROGRAMS="base64 cat chcon chgrp chown cksum comm cut date df dircolors dirname du factor fold mknod pr seq tr uname"
 #PROGRAMS="env factor chroot date fold expand csplit join head"
 #PROGRAMS="csplit date expand factor fold join tr seq pr mknod"
-PROGRAMS="base64 cut dd du expand fold mknod tr tsort unexpand"
+#PROGRAMS="base64 cut dd du expand fold mknod tr tsort unexpand"
+PROGRAMS="basename chcon cksum comm dircolors dirname env \
+  expr groups link logname mkdir mkfifo nice nl wc unlink \
+  touch tee sync sum split sleep setuidgid rmdir readlink \
+  printf pathchk paste od"
+
 
 for program in $PROGRAMS
 do
