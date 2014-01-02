5e279e5c2f93a3a9244300aecfbb6242eae58ccc
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2014-01-02 09:21:44 +0000

    modify analysis.sh for all coreutils

--- analysis.sh
+++ analysis.sh
@@ -3,7 +3,8 @@
 #PROGRAMS="env factor chroot date fold expand csplit join head"
 #PROGRAMS="csplit date expand factor fold join tr seq pr mknod"
 #PROGRAMS="base64 cut dd du expand fold mknod tr tsort unexpand"
-PROGRAMS="basename chcon cksum comm dircolors dirname env \
+PROGRAMS="base64 cut dd du expand fold mknod tr tsort unexpand \
+  basename chcon cksum comm dircolors dirname env \
   expr groups link logname mkdir mkfifo nice nl wc unlink \
   touch tee sync sum split sleep setuidgid rmdir readlink \
   printf pathchk paste od"
