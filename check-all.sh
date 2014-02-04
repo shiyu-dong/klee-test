aede46c59764856d76abcc361d64a704c99ca4db
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@eaa8fc15-aa47-96ba-b1e6-f9b0d34e8033>
2014-02-04 21:51:41 +0000

    running script for compaing solver done

--- check-all.sh
+++ check-all.sh
@@ -9,17 +9,21 @@ du env expand expr fold groups link logname mkdir mkfifo mknod nice nl \
 od paste pathchk printf readlink rmdir setuidgid sleep split sum \
 sync tee touch tr tsort unexpand unlink wc"
 
-SOLVER="[ base64 chmod comm csplit dircolors echo env factor join ln mkfifo"
+SOLVER="base64 chmod comm csplit dircolors echo env factor join ln mkfifo"
+# ignoring "["
 
 if [ "$1" == "--clean" ]
 then
   ./compare-all.sh --clean
 else
   if [ ! -d "test_result_new" ]; then
-    mkdir test_result_new 
+    mkdir test_result_new
   fi
-  for program in $PROGRAMS
+  for program in $SOLVER
   do
-    ./compare-all.sh $program &> test_result_new/result_$program.txt
+    for solver in stp z3 btor
+    do
+      ./compare-all.sh $program 1200 $solver &> test_result_new/result_${program}_${solver}.txt
+    done
   done
 fi
--- compare-all.sh
+++ compare-all.sh
@@ -1,89 +1,124 @@
+# $1 program
+# $2 time
+# $3 solver (or none)
+
 FLAG1="\
-RaiseAllocation,\
-ScalarReplAggregates,\
-MemCpyOpt,\
-InstructionCombining,\
-CondPropagation,\
-DeadArgElimination,\
-DeadStoreElimination,\
-DeadTypeElimination,\
-FunctionAttrs,\
-GlobalDCE,\
-GVN,\
-IPConstantPropagation,\
-LICM,\
-LoopDeletion,\
-LoopIndexSplit,\
-LoopRotate,\
-LoopUnswitch,\
-PruneEH,\
-Reassociate,\
-SimplifyLibCalls,\
-SCCP,\
-StripDeadPrototypes,\
-CFGSimplification"
+  RaiseAllocation,\
+  ScalarReplAggregates,\
+  MemCpyOpt,\
+  InstructionCombining,\
+  CondPropagation,\
+  DeadArgElimination,\
+  DeadStoreElimination,\
+  DeadTypeElimination,\
+  FunctionAttrs,\
+  GlobalDCE,\
+  GVN,\
+  IPConstantPropagation,\
+  LICM,\
+  LoopDeletion,\
+  LoopIndexSplit,\
+  LoopRotate,\
+  LoopUnswitch,\
+  PruneEH,\
+  Reassociate,\
+  SimplifyLibCalls,\
+  SCCP,\
+  StripDeadPrototypes,\
+  CFGSimplification"
 
 FLAG2="\
-RaiseAllocation,\
-ScalarReplAggregates,\
-MemCpyOpt,\
-InstructionCombining,\
-DeadStoreElimination,\
-DeadTypeElimination,\
-FunctionAttrs,\
-GlobalDCE,\
-IPConstantPropagation,\
-LoopDeletion,\
-LoopRotate,\
-PruneEH,\
-Reassociate,\
-StripDeadPrototypes"
+  RaiseAllocation,\
+  ScalarReplAggregates,\
+  MemCpyOpt,\
+  InstructionCombining,\
+  DeadStoreElimination,\
+  DeadTypeElimination,\
+  FunctionAttrs,\
+  GlobalDCE,\
+  IPConstantPropagation,\
+  LoopDeletion,\
+  LoopRotate,\
+  PruneEH,\
+  Reassociate,\
+  StripDeadPrototypes"
 
 FLAG3="\
-PromoteMemoryToRegister,\
-ScalarReplAggregates,\
-IndVarSimplify,\
-LoopRotate,\
-InstructionCombining,\
-PromoteMemoryToRegister"
+  PromoteMemoryToRegister,\
+  ScalarReplAggregates,\
+  IndVarSimplify,\
+  LoopRotate,\
+  InstructionCombining,\
+  PromoteMemoryToRegister"
 
 FLAG4="\
-PromoteMemoryToRegister,\
-InstructionCombining,\
-ScalarReplAggregates,\
-InstructionCombining,\
-LoopRotate,\
-InstructionCombining,\
-IndVarSimplify,\
-InstructionCombining,\
-ScalarReplAggregates,\
-InstructionCombining,\
-PromoteMemoryToRegister"
+  PromoteMemoryToRegister,\
+  InstructionCombining,\
+  ScalarReplAggregates,\
+  InstructionCombining,\
+  LoopRotate,\
+  InstructionCombining,\
+  IndVarSimplify,\
+  InstructionCombining,\
+  ScalarReplAggregates,\
+  InstructionCombining,\
+  PromoteMemoryToRegister"
 
 FLAG5="\
-PromoteMemoryToRegister,\
-InstructionCombining,\
-ScalarReplAggregates,\
-InstructionCombining"
+  PromoteMemoryToRegister,\
+  InstructionCombining,\
+  ScalarReplAggregates,\
+  InstructionCombining"
 
 FLAG6="\
-LoopRotate,\
-IndVarSimplify,\
-PromoteMemoryToRegister,\
-InstructionCombining,\
-ScalarReplAggregates,\
-InstructionCombining"
+  LoopRotate,\
+  IndVarSimplify,\
+  PromoteMemoryToRegister,\
+  InstructionCombining,\
+  ScalarReplAggregates,\
+  InstructionCombining"
 
 rm -rf ../klee-out-* ../klee-last
 if [ "$1" != "--clean" ]
 then
-  for SOLVER in stp z3 btor
+  # without optimization
+  echo "=============================================="
+  echo "no optimization"
+  echo "=============================================="
+  ./run-a-test.sh $1 $2 $3 none none
+  klee-stats --print-all ../klee-last
+
+  cd ../../../obj-gcov/src/
+  rm -rf *.gcda
+  klee-replay $1 ../../obj-llvm/src/klee-last/*.ktest
+  gcov -b -c $1
+  cd ../../obj-llvm/src/klee-test
+
+  # with original klee optimization
+  echo "=============================================="
+  echo "original optimization"
+  echo "=============================================="
+  ./run-a-test.sh $1 $2 $3 optimize none
+  klee-stats --print-all ../klee-last
+
+  cd ../../../obj-gcov/src/
+  rm -rf *.gcda
+  klee-replay $1 ../../obj-llvm/src/klee-last/*.ktest
+  gcov -b -c $1
+  cd ../../obj-llvm/src/klee-test
+
+  # with selected opt-flag
+  for OPT in \
+    IndVarSimplify \
+    InstructionCombining \
+    LoopRotate \
+    PromoteMemoryToRegister \
+    ScalarReplAggregates
   do
-    # without optimization
     echo "=============================================="
-    echo "no optimization"
+    echo "with optimization flag "${OPT}
     echo "=============================================="
-    ./run-a-test.sh $1 null null ${SOLVER}
+    ./run-a-test.sh $1 $2 $3 optimize ${OPT}
     klee-stats --print-all ../klee-last
 
     cd ../../../obj-gcov/src/
@@ -91,40 +126,5 @@ then
     klee-replay $1 ../../obj-llvm/src/klee-last/*.ktest
     gcov -b -c $1
     cd ../../obj-llvm/src/klee-test
-
-    # with original klee optimization
-    echo "=============================================="
-    echo "original optimization"
-    echo "=============================================="
-    ./run-a-test.sh $1 optimize null ${SOLVER}
-    klee-stats --print-all ../klee-last
-
-    cd ../../../obj-gcov/src/
-    rm -rf *.gcda
-    klee-replay $1 ../../obj-llvm/src/klee-last/*.ktest
-    gcov -b -c $1
-    cd ../../obj-llvm/src/klee-test
-
-
-    # with selected opt-flag
-    for OPT in \
-      IndVarSimplify \
-      InstructionCombining \
-      LoopRotate \
-      PromoteMemoryToRegister \
-      ScalarReplAggregates \
-    do
-      echo "=============================================="
-      echo "with optimization flag "${OPT}
-      echo "=============================================="
-      ./run-a-test.sh $1 optimize ${OPT} ${SOLVER}
-      klee-stats --print-all ../klee-last
-
-      cd ../../../obj-gcov/src/
-      rm -rf *.gcda
-      klee-replay $1 ../../obj-llvm/src/klee-last/*.ktest
-      gcov -b -c $1
-      cd ../../obj-llvm/src/klee-test
-    done
   done
 fi
